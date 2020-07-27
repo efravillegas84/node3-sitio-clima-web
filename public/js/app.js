b37271e9dd1ce3d347e61a98c8787acb748c1b8b
efravillegas84 <efravillegas84@gmail.com>
2020-07-27 01:18:50 -0400

    se extiende la informacion de clima

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,12 @@
 const climaForm = document.querySelector('form')
 const busquedaInput = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
-const msg2 = document.querySelector('#msg2')
+const clima = document.querySelector('#clima')
+const temperatura = document.querySelector('#temperatura')
+const min = document.querySelector('#min')
+const max = document.querySelector('#max')
+const presion = document.querySelector('#presion')
+const humedad = document.querySelector('#humedad')
 
 climaForm.addEventListener('submit', (e)=>{
     e.preventDefault()
@@ -9,8 +14,13 @@ climaForm.addEventListener('submit', (e)=>{
 
     url = '/clima?direccion='+direccion
     
-    msg1.textContent = 'Loading...'
-    msg2.textContent = ''
+    msg1.textContent        = 'Loading...'
+    clima.textContent       = ''
+    temperatura.textContent = ''
+    min.textContent         = ''
+    max.textContent         = ''
+    presion.textContent     = ''
+    humedad.textContent     = ''
 
     fetch(url).then((response)=>{
         response.json().then((data)=>{
@@ -18,8 +28,13 @@ climaForm.addEventListener('submit', (e)=>{
                 msg1.textContent = data.error
             }
             else{
-                msg1.textContent = 'Ciudad: ' + data.ciudad
-                msg2.textContent = 'Clima: ' + data.clima
+                msg1.textContent        = 'Ciudad: ' + data.ciudad
+                clima.textContent       = 'Clima: ' + data.clima.clima
+                temperatura.textContent = 'Temperatura: ' + data.clima.temp+'°'
+                min.textContent         = 'Temp min: ' + data.clima.temp_min+'°'
+                max.textContent         = 'Temp max: ' + data.clima.temp_max+'°'
+                presion.textContent     = 'Presion: ' + data.clima.presion+'Pa'
+                humedad.textContent     = 'Humedad: ' + data.clima.humedad+'%hr'
             }
         })
     })
--- src/utils/climaService.js
+++ src/utils/climaService.js
@@ -3,6 +3,7 @@ const request = require('request')
 const climaCordenadas = (latitud, longitud, callback)=>{
     // const url='http://api.weatherstack.com/current?access_key=f6f2c88157bd2b59b0a0133837b8c1e6&query=-33.433612,-70.638696'
     const url='http://api.openweathermap.org/data/2.5/weather?lat='+latitud+'&lon='+longitud+'&appid=64622ffc08658773de518be75b5c3b2a&units=metric&lang=es'
+    
     request({url, json:true}, (error, {body}={})=>{
         if(error){
             callback('servicio no se pudo contactar:['+error+']', undefined);
@@ -11,7 +12,14 @@ const climaCordenadas = (latitud, longitud, callback)=>{
             callback('ERROR EN PETICION:['+body.message+']', undefined);
         }
         else{
-            callback(undefined, body.weather[0].description + ' y la temperatura actual es '+body.main.temp)
+            callback(undefined,{
+                clima: body.weather[0].description,
+                temp: body.main.temp,
+                temp_min: body.main.temp_min,
+                temp_max: body.main.temp_max,
+                presion: body.main.pressure,
+                humedad: body.main.humidity,
+            })
         }
     })
 }

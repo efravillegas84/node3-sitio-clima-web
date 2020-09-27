2d136ee7a5571e252874ad5c69b85e6f260f7644
efravillegas84 <efravillegas84@gmail.com>
2020-09-27 07:15:42 -0300

    se reordena verificarCaptcha

--- src/utils/recaptchav2.js
+++ src/utils/recaptchav2.js
@@ -3,7 +3,8 @@ const https =require('https')
 var secretKey = "6LeZn74ZAAAAAHnPwT73JcZlN_eCnWMtW6rfc8bj";
 
 function verificarCaptcha(key, callback) {
-    https.get("https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + key, function(res) {
+        const url = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + key
+        https.get(url, function(res) {
             var data = "";
             res.on('data', function (chunk) {
                     data += chunk.toString();
